05d046635c84034fae1ed3663134adb4a3518869
Guardeek <guardeec@gmail.com>
2017-10-25 20:55:06 +0300

    10_2

--- src/lesson13/task1/Endstring.java
+++ src/lesson13/task1/Endstring.java
@@ -6,11 +6,13 @@ public class Endstring {
         System.out.println("Last symbol:" + str1.charAt(lengs - 1));
         System.out.println("Tree symboll:" + str1.endsWith("!!!"));
         System.out.println("Tree symboll:" + str1.startsWith("I like"));
+        System.out.println("lastIndexOf('Java') = " + str1.lastIndexOf("Java"));
+        System.out.println(str1.replace('a', 'o'));
+        System.out.println(str1.toUpperCase());
+        System.out.println(str1.toLowerCase());
     }
 
     public static void main(String[] args) {
         end("I like Java!!!");
     }
-
-
 }
