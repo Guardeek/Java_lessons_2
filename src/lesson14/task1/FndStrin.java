b3fb92b99457f59542e8fa1ddc9449bdb77dbc67
Guardeek <guardeec@gmail.com>
2017-10-29 10:40:31 +0200

    10_2

--- src/lesson14/task1/FndStrin.java
+++ src/lesson14/task1/FndStrin.java
@@ -1,11 +1,18 @@
 package lesson14.task1;
 
-import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class FndStrin  {
-    public static void main(String[] args) {
-        String str = "Versions: Java  5, Java 6, Java   7, Java 8.";
-        String[] words = str.split("\\\\d+\\\\s?");
-        System.out.print(Arrays.toString(words));
+
+    public static void main(String args[]) {
+
+        String stringPattern = "[1-z]+";
+        String text = "Versions: Java  5, Java 6, Java   7, Java 8.";
+        Pattern pattern = Pattern.compile(stringPattern);
+        Matcher matcher = pattern.matcher(text);
+        while (matcher.find()) {
+            System.out.println(text.substring(matcher.start(), matcher.end()));
+        }
     }
 }
