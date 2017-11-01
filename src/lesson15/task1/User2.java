f4805849fe3e00d90c4f3cf405e2efcfe40e08b4
Guardeek <guardeec@gmail.com>
2017-11-01 20:24:51 +0200

    10_2

--- src/lesson15/task1/User2.java
+++ src/lesson15/task1/User2.java
@@ -4,25 +4,20 @@ public class User2 {
     private static String login = "qwerty";
     private static String password = "111111";
 
-    public class Query {
-        public void printToLogg() {
-            System.out.println("User is " + User2.this);
-            System.out.println("login is " + login);
-            System.out.println("password is " + password);
-        }
-    }
+    public static class Query {
+      //  class Query {
+            public void printToLogg() {
 
-    public void createQuery() {
+                System.out.println("login is " + login);
+                System.out.println("password is " + password);
+            }
+        }
+
         Query query = new Query();
-        query.printToLogg();
-    }
+   //     query.printToLogg();
+
 
     public static void main(String[] args) {
-        User2 user2 = new User2();
-        user2.createQuery();
-        User2.Query query1 = user2.new Query();
-        User2.Query query2 = new User2().new Query();
-        query1.printToLogg();
-        query2.printToLogg();
+
     }
 }
