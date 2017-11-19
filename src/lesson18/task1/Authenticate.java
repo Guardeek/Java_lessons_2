9b303fb34065c8f112878cb3db9d767a0cbc60d8
Guardeek <guardeec@gmail.com>
2017-11-19 10:13:13 +0200

    10_2

--- src/lesson18/task1/Authenticate.java
+++ src/lesson18/task1/Authenticate.java
@@ -1,26 +1,29 @@
 package lesson18.task1;
 
 public class Authenticate {
-    public static void main(String[] args)  {
-        try {
-        authenticate("myLogin", "myPassword", "myPassword");
-        authenticate("myLoginmyLoginmyLogin", "myPassword", "myPassword");
-        authenticate("myLogin", "myPasswordmyPasswordmyPassword", "myPasswordmyPasswordmyPassword");
-        authenticate("myLogin", "myPassword", "myPassword2");
-        } catch (WrongLoginException e) {
-            e.printStackTrace();
-        } catch (WrongPasswordException e) {
-           System.out.println(e.getMessage());
-        }
+    public static void main(String[] args) {
+        System.out.println(authenticate("myLogin", "myPassword", "myPassword") + "\n");
+        System.out.println(authenticate("myLoginmyLoginmyLogin", "myPassword", "myPassword") + "\n");
+        System.out.println(authenticate("myLogin", "myPasswordmyPasswordmyPassword", "myPasswordmyPasswordmyPassword") + "\n");
+        System.out.println(authenticate("myLogin", "myPassword", "myPassword2") + "\n");
     }
 
-    private static boolean authenticate(String login, String password, String confpassword)
-            throws WrongLoginException, WrongPasswordException {
-        if (login.length() < 20) {
-            throw new WrongPasswordException("Login more than 20.");
-        }
-        if (password.length() > 20 || !password.equals(confpassword)) {
-            throw new WrongPasswordException("Password more than 20.");
+    private static boolean authenticate(String login, String password, String confpassword) {
+        try {
+            if (login.length() < 20) {
+                throw new WrongLoginException("Login more than 20.");
+            }
+            if (password.length() > 20 || !password.equals(confpassword)) {
+                throw new WrongPasswordException("Password more than 20.");
+            }
+        } catch (WrongPasswordException e) {
+            System.out.println(e.getMessage());
+            return false;
+        } catch (WrongLoginException e) {
+            System.out.println(e.getMessage());
+            return false;
+        } finally {
+            System.out.println();
         }
         return true;
     }
